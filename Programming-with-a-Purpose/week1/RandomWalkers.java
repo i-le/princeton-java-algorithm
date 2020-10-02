986ff41c8d20030de1cf0fa0a845d5728e1a3779
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2020-10-01 21:45:45 -0400

    Add files via upload
    week2

---
+++ Programming-with-a-Purpose/week1/RandomWalkers.java
@@ -0,0 +1,37 @@
+public class RandomWalkers {
+    public static void main(String[] args) {
+        int r = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        int step = 0;
+
+        for (int j = 0; j < trials; j++) {
+
+            int x = 0;
+            int y = 0;
+            //int random = (int) (ra * 4);
+            // System.out.println(ra + " " + random);
+
+            for (int i = 0; (Math.abs(x) + Math.abs(y)) < r; i++) {
+                double random1 = Math.random();
+                int random = (int) (random1 * 4);
+
+                if (random < 1) {
+                    x += 1;
+                } else if (random < 2) {
+                    x -= 1;
+                } else if (random < 3) {
+                    y += 1;
+                } else if (random < 4) {
+                    y -= 1;
+                }
+                //System.out.println("(" + x + "," + y + ")");
+                step += 1;
+            }
+
+            //System.out.println("steps = " + step);
+
+        }
+        System.out.println("average number of steps = " + (double) step / trials);
+
+    }
+}

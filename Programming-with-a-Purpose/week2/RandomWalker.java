094e55268c1d87b1b7f607731f1b9e18dc8028ac
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2020-10-02 20:37:46 -0400

    Add files via upload
    week2

---
+++ Programming-with-a-Purpose/week2/RandomWalker.java
@@ -0,0 +1,27 @@
+public class RandomWalker {
+    public static void main(String[] args) {
+        int r = Integer.parseInt(args[0]);
+        int x = 0;
+        int y = 0;
+        int steps = 0;
+        for (int i = 0; (Math.abs(x) + Math.abs(y)) < r; i++) {
+            System.out.println("(" + x + "," + y + ")");
+            double random = Math.random();
+            int dirc = (int) (random * 4);
+
+            if (dirc < 1) {
+                x += 1;
+            } else if (dirc < 2) {
+                x -= 1;
+            } else if (dirc < 3) {
+                y += 1;
+            } else if (dirc < 4) {
+                y -= 1;
+            }
+            steps += 1;
+        }
+        System.out.println("(" + x + "," + y + ")");
+
+        System.out.println("steps = " + steps);
+    }
+}
